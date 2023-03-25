8e29f61e53e1a2eb9a55cf73db1dfc456bb0204a
jimaa maya <jimaa.maya@gmail.com>
2023-03-25 12:42:29 +0300

    phase-1-array-filter-method-lab-done

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution here
+
+const findMatching =function(arr, expectedName) {
+    return arr.filter(name => name.toLowerCase() === expectedName.toLowerCase())
+};
+
+const fuzzyMatch = function(arr, searchString) {
+    return arr.filter(name => name.slice(0, searchString.length) ===searchString)
+};
+
+
+const matchName = function(arr, expectedName) {
+    return arr.filter(item => item.name === expectedName)
+};
